8a9c8302692e0c05fcd26ba3713b23c597a1e231
Ivan <ivanprokopets1999@gmail.com>
2019-10-29 17:35:20 +0100

    Fourth code refactoring

--- TDD/src/main/java/ArabicToRomanNumberConverter.java
+++ TDD/src/main/java/ArabicToRomanNumberConverter.java
@@ -1,15 +1,21 @@
 public class ArabicToRomanNumberConverter {
-    public String Convert (int arabicNumber) {
+    public String Convert(int arabicNumber) {
         boolean noNumber = arabicNumber == 0;
         if (noNumber)
             return getDefaultRomanNumber();
-        if (arabicNumber == 1)
-            return "I";
-        else if (arabicNumber == 2)
-            return "II";
-        else return "III";
+
+        return getRepeatingRomanNumber(arabicNumber);
     }
 
+    private String getRepeatingRomanNumber(int arabicNumber) {
+        String romanNumber = "";
+        while (arabicNumber >= 1) {
+            romanNumber += "I";
+            arabicNumber--;
+        }
+        return romanNumber;
+    }
+    
     private String getDefaultRomanNumber() {
         return "";
     }
--- TDD/src/test/java/WhenConvertArabicNumberToRomanTest.java
+++ TDD/src/test/java/WhenConvertArabicNumberToRomanTest.java
@@ -1,11 +1,13 @@
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class WhenConvertArabicNumberToRomanTest {
-    private String convertToRoman ( int arabicNumber) {
+    private String convertToRoman(int arabicNumber) {
         ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
         return converter.Convert(arabicNumber);
     }
+
     @Test
     public void Convert_0_To_EmptyString() {
         String romanNumber = convertToRoman(0);
